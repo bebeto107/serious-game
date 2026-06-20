383ba78384c147b5d2320860595ad7348d7062c3
Ralsei sony <felipeccti2024@gmail.com>
2026-06-20 17:56:44 -0400

    bah

--- enemy.gd
+++ enemy.gd
@@ -1,19 +1,20 @@
 extends CharacterBody2D
-
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var player = null
+var player_chase= false
 
 
 func _physics_process(delta: float) -> void:
-	
+	#if player_chase == true :
+		#position += (player.position- position)/( Global.speed*30)
+		pass
+func _on_detection_body_entered(body) -> void:
+	player = body
+	player_chase= true
 
-	move_and_slide()
+func _on_detection_body_exited(body) -> void:
+	player=null
+	player_chase=false
 
 
-func _on_detection_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
-
-
-func _on_detection_body_exited(body: Node2D) -> void:
+func _on_damage_area_entered(area: Area2D) -> void:
 	pass # Replace with function body.
