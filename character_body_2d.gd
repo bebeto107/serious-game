d16ff5df7dea49d989c7c69629f21a0bb3b3144f
Ralsei sony <felipeccti2024@gmail.com>
2026-06-20 17:11:45 -0400

    nem eu sei o que eu fiz

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,12 +3,20 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var current_direction = rotation
 
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("mouse_left"):
 		rotation+=0.1
-	else:	
-		position += transform.y * SPEED * delta*-1
+		Global.speed=0.2
+		
+	else: 
+		Global.speed=1
+		position += transform.y * SPEED * delta*-1* Global.speed
+	if Input.is_action_just_released("mouse_left"):
+		print(self.get_rotation() )
+	
+
 	
 	move_and_slide()
